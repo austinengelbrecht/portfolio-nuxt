97468df3dc63d22e7e1e8503ab8853450a0a3ec1
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-05-30 20:32:04 -0500

    Setup base styles and fix styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,20 @@
     <slot />
   </body>
 </template>
+
+<style lang="scss">
+html,
+body {
+  background-color: #03001c;
+  padding: 0;
+  margin: 0;
+  font-family: "Source Sans Pro", sans-serif;
+  color: #efedff;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
 <template>
-  <HomeHero />
-  <HomeAbout />
+  <div class="intro">
+    <HomeHero />
+    <HomeAbout />
+  </div>
 </template>
+
+<style lang="scss">
+.intro {
+  background: linear-gradient(165deg, rgb(3, 0, 28) 0%, rgb(48, 30, 103) 100%);
+}
+</style>
