fce25812f598385b8f44d99cac2625b07e063ef1
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-18 14:26:43 -0500

    Convert to section

--- components/HomeHeroNew.vue
+++ components/HomeHeroNew.vue
@@ -1,30 +1,34 @@
 <template>
-  <img
-    class="about-img"
-    src="https://npjmxgrfqyrxnbnmltws.supabase.co/storage/v1/object/public/images/austinengelbrecht.jpg"
-    width="400"
-    height="400"
-    loading="lazy"
-  />
-
-  <div>
+  <section>
     <img
-      class="hero-title-img"
-      src="../assets/svg/title_graphic.svg"
-      alt="Hi, I'm Austin"
-      width="715"
-      height="445"
+      class="about-img"
+      src="https://npjmxgrfqyrxnbnmltws.supabase.co/storage/v1/object/public/images/austinengelbrecht.jpg"
+      width="400"
+      height="400"
+      loading="lazy"
     />
 
-    <h2 class="about-heading">About Austin</h2>
-    <p class="about-content">
-      While getting my Information Science degree, I found my passion for
-      building websites and working in front-end code. I am a self-motivated
-      developer striving to improve my skills, whether it's through online
-      courses, attending conferences, or working on personal projects, I am
-      constantly striving to learn and grow as a developer.
-    </p>
+    <div>
+      <img
+        class="hero-title-img"
+        src="../assets/svg/title_graphic.svg"
+        alt="Hi, I'm Austin"
+        width="715"
+        height="445"
+      />
 
-    <HeroButton url="/" text="Learn More" />
-  </div>
+      <h2 class="about-heading">About Austin</h2>
+      <p class="about-content">
+        While getting my Information Science degree, I found my passion for
+        building websites and working in front-end code. I am a self-motivated
+        developer striving to improve my skills, whether it's through online
+        courses, attending conferences, or working on personal projects, I am
+        constantly striving to learn and grow as a developer.
+      </p>
+
+      <HeroButton url="/" text="Learn More" />
+    </div>
+  </section>
 </template>
+
+<style lagn="scss"></style>
