b9f5d5dddc776db3655779160ee26a4c08699f68
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-06-27 22:25:49 -0500

    Add basic prop setup

--- components/ui/ImageLoader.vue
+++ components/ui/ImageLoader.vue
@@ -1,5 +1,29 @@
-<template></template>
+<template>
+  <div>
+    <img :src="source" :alt="altText" :aria-hidden="hidden" />
+  </div>
+</template>
 
-<script></script>
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    altText: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    hidden: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+};
+</script>
 
 <style></style>
