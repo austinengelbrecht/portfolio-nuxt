f17e4f4a4b4e7fa0f6458b38e01b51990bb51cd3
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-07-05 12:08:34 -0500

    start about page

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>About Me</h1>
+  <section class="about">
+    <h1>About Me</h1>
+    <p>Here is some generic text about me</p>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  max-width: 70%;
+  margin: 0 auto;
+}
+</style>
