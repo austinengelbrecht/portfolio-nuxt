68f0c387fd946952dc062ee1e012f6938ccaa866
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-07-05 20:06:16 -0500

    Add about me intro

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -1,8 +1,19 @@
 <template>
-  <section class="about">
-    <h1>About Me</h1>
-    <p>Here is some generic text about me</p>
-  </section>
+  <div class="about">
+    <section>
+      <h1>About Me</h1>
+      <p>
+        I had my first experience with web development years ago when I built a
+        small website as a class project. The first website I coded was rough
+        around the edges, but it sent me down a path of continuous learning and
+        development. Now, almost four years later, I am continuing to learn
+        little by little every day; finding myself naturally curious with a
+        drive to continue growing my skills in web development.
+      </p>
+    </section>
+
+    <section></section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
