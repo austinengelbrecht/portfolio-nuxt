78e5fa9e8327a7d2658c2a189bf17114a6bd78ac
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-07-08 17:27:53 -0500

    Rewrite and edits

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -4,11 +4,11 @@
       <h1>About Me</h1>
       <p>
         I had my first experience with web development years ago when I built a
-        small website as a class project. The first website I coded was rough
-        around the edges, but it sent me down a path of continuous learning and
-        development. Now, almost four years later, I am continuing to learn
-        little by little every day; finding myself naturally curious with a
-        drive to continue growing my skills in web development.
+        small website as a class project. The first website I coded was crude,
+        yet it ignited a spark of continuous learning and development. Now,
+        almost four years later, I am continuing to learn little by little every
+        day; finding myself naturally curious with a drive to continue growing
+        my skills in web development.
       </p>
       <p>
         Professoinally, I primarily work with Elm and SCSS to build front-end
@@ -24,16 +24,15 @@
 
     <section>
       <h2>Outside of the Office</h2>
-
       <p>
-        Outside of work I have many different interests. I enjoy lifting
-        weights, and try to do so approx. 3 times a week. I also enjoy the
-        occasional hike to get outside and experience the beauty of nature. And
+        Outside of work I have many different interests. I enjoy lifting weights
+        to relief stress and keep healthy. I also enjoy the occasional hike or
+        bike ride to get outside and experience the beauty of nature. And
         recently, I have been learning how to fly-fish on the weekends.
       </p>
       <p>
         When the weather doesn't let me get outside, I enojoy playing casual
-        video games with my friends. Or curling up on the couch and watching a
+        video games with my friends, or curling up on the couch and watching a
         good movie or TV show.
       </p>
     </section>
