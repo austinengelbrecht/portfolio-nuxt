35e11ac091f3e95ac69fa6881db234f78a2191ca
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-07-08 22:24:17 -0500

    Add meta keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,13 @@ export default defineNuxtConfig({
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       title: "Austin Engelbrecht",
+      meta: [
+        {
+          name: "keywords",
+          content:
+            "austinengelbrecht.com, austin engelbrecht, developer, front-end developer, ux, ui",
+        },
+      ],
     },
   },
 });
