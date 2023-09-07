c4503b5099163cd90f871299946b050e84000cfe
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-06 22:40:29 -0500

    Create basic form

---
+++ pages/contactme.vue
@@ -0,0 +1,72 @@
+<template>
+  <section>
+    <h1>Contact Me</h1>
+    <p>I would love to hear from you.</p>
+
+    <form @prevent.default="validateForm">
+      <div class="contact">
+        <label class="contact-input">
+          Your name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            v-model="name"
+          />
+        </label>
+
+        <label class="contact-input">
+          Your email
+          <input
+            type="text"
+            name="email"
+            placeholder="Your Email"
+            v-model="email"
+          />
+        </label>
+
+        <label class="contact-input">
+          Your message
+          <input
+            type="text"
+            name="message"
+            placeholder="Your Message"
+            v-model="message"
+          />
+        </label>
+      </div>
+
+      <button type="submit">Submit Form</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+  methods: {
+    submitForm() {},
+    validateForm() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  display: flex;
+  flex-direction: column;
+  max-width: 700px;
+
+  &-input {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 32px;
+  }
+}
+</style>
