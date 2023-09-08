71e5bb11bfcd115043591c494a0ceebaa1dd3363
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-07 21:45:54 -0500

    Create stylesheet

--- pages/contactme.vue
+++ pages/contactme.vue
@@ -1,40 +1,43 @@
 <template>
   <section class="flex justify-center p-4">
-    <div>
+    <div class="contactme">
       <h1>Contact Me</h1>
       <p>I would love to hear from you.</p>
 
-      <form @prevent.default="validateForm" class="">
+      <form @prevent.default="validateForm" class="contact-form">
         <div>
-          <label for="name" class="contact-input"> Your name</label>
+          <label for="name" class="label"> Your name</label>
           <input
             id="name"
             type="text"
             name="name"
             placeholder="Your Name"
             v-model="name"
+            class="contact-input"
           />
         </div>
 
         <div>
-          <label for="email" class="contact-input">Your email</label>
+          <label for="email" class="label">Your email</label>
           <input
             id="email"
             type="text"
             name="email"
             placeholder="Your Email"
             v-model="email"
+            class="contact-input"
           />
         </div>
 
         <div>
-          <label for="message" class="contact-input">Your Message</label>
+          <label for="message" class="label">Your Message</label>
           <input
             id="message"
             type="text"
             name="message"
             placeholder="Your Message"
             v-model="message"
+            class="contact-input"
           />
         </div>
 
@@ -60,4 +63,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/styles/partials/contactme/contact.scss";
+</style>
