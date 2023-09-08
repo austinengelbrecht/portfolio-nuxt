f93797c048350b5e343237a4a2b31b4c38d5fb12
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-07 22:00:50 -0500

    More style work

--- pages/contactme.vue
+++ pages/contactme.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="flex justify-center p-4">
-    <div class="contactme">
+    <div class="contactme w-[100%]">
       <h1>Contact Me</h1>
-      <p>I would love to hear from you.</p>
+      <p class="mb-20">I would love to hear from you.</p>
 
-      <form @prevent.default="validateForm" class="contact-form">
+      <form @prevent.default="validateForm">
         <div class="form-field">
           <input id="name" type="text" v-model="name" class="contact-input" />
-          <label for="name" class="label"> Your name</label>
+          <label for="name" class="label"> Your Name</label>
         </div>
 
         <div class="form-field">
           <input id="email" type="text" v-model="email" class="contact-input" />
-          <label for="email" class="label">Your email</label>
+          <label for="email" class="label">Your Email</label>
         </div>
 
         <div class="form-field">
