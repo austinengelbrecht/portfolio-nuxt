91793c1add8150a4982d852cb76da4ce2e3de2a5
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-10 02:53:30 -0500

    Comeback to writing tests

--- tests/contactme.test.js
+++ tests/contactme.test.js
@@ -1,18 +1,23 @@
-import { it, expect, describe } from "vitest";
-// ! This package is currently not installed
-// import { mount } from "@vue/test-utils";
-import contactme from "~/pages/contactme.vue";
+// import { it, expect, describe } from "vitest";
+// import { shallowMount } from "@vue/test-utils";
+// import contactme from "~/pages/contactme.vue";
 
 // describe("ContacMe.vue", () => {
-//   it("Should render the contact form", () => {
-//     const wrapper = mount(contactme);
+//   it("Should render a blank contact form", () => {
+//     const wrapper = shallowMount(contactme, {
+//       formData: {
+//         name: "",
+//         email: "",
+//         message: "",
+//       },
+//     });
 
-//     const contact = wrapper.get("");
+//     expect(wrapper).toBeTruthy();
 //   });
 // });
 
-describe("Contact Form Validation", () => {
-  it("Form is invalid if no name if provided", () => {
-    const name = "";
-  });
-});
+// describe("Contact Form Validation", () => {
+//   it("Form is invalid if no name if provided", () => {
+//     const name = "";
+//   });
+// });
